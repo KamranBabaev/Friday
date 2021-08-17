fcc5952df51facc80d7f35a345eaae0604957c2a
Kamran Babaev <babaevkamran2094@gmail.com>
2021-08-17 16:03:38 +0300

    add gh pages and start components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import SuperInputText
-    from './components/common/c1-SuperInputText/SuperInputText';
+  from './components/common/c1-SuperInputText/SuperInputText';
 import SuperCheckbox from './components/common/c3-SuperCheckbox/SuperCheckbox';
 import SuperButton from './components/common/c2-SuperButton/SuperButton';
 import {Route, Switch} from 'react-router-dom';
@@ -13,30 +13,30 @@ import {InputPassword} from './components/InputPassword/InputPassword';
 import {Error404} from './components/Error404/Error404';
 
 function App() {
-    return (
-        <div className="App">
-            <Switch>
-                <Route path={'/login'}
-                       render={() => <Login/>}/>
-                <Route path={'/profile'}
-                       render={() => <Profile/>}/>
-                <Route path={'/registration'}
-                       render={() => <Registration/>}/>
-                <Route path={'/restorpassword'}
-                       render={() => <RestorPassword/>}/>
-                <Route path={'/testPage'}
-                       render={() => <TestPage/>}/>
-                <Route path={'/inputpassword'}
-                       render={() => <InputPassword/>}/>
-                <Route path={'*'}
-                       render={() => <Error404/>}/>
-            </Switch>
+  return (
+      <div className="App">
+        <Switch>
+          <Route path={'/login'}
+                 render={() => <Login/>}/>
+          <Route path={'/profile'}
+                 render={() => <Profile/>}/>
+          <Route path={'/registration'}
+                 render={() => <Registration/>}/>
+          <Route path={'/restorpassword'}
+                 render={() => <RestorPassword/>}/>
+          <Route path={'/testPage'}
+                 render={() => <TestPage/>}/>
+          <Route path={'/inputpassword'}
+                 render={() => <InputPassword/>}/>
+          <Route path={'*'}
+                 render={() => <Error404/>}/>
+        </Switch>
 
-            <SuperInputText/>
-            <SuperCheckbox/>
-            <SuperButton/>
-        </div>
-    );
+        <SuperInputText/>
+        <SuperCheckbox/>
+        <SuperButton/>
+      </div>
+  );
 }
 
 export default App;
