206d4e4799fa0eddbb31837bba27824c85cb4604
Kamran Babaev <babaevkamran2094@gmail.com>
2021-08-17 15:53:17 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SuperInputText
+    from './components/common/c1-SuperInputText/SuperInputText';
+import SuperCheckbox from './components/common/c3-SuperCheckbox/SuperCheckbox';
+import SuperButton from './components/common/c2-SuperButton/SuperButton';
+import {Route, Switch} from 'react-router-dom';
+import {Login} from './components/Login/Login';
+import {Profile} from './components/Profile/Profile';
+import {Registration} from './components/Registration/Registration';
+import {RestorPassword} from './components/RestorPassword/RestorPassword';
+import {TestPage} from './components/TestPage/TestPage';
+import {InputPassword} from './components/InputPassword/InputPassword';
+import {Error404} from './components/Error404/Error404';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Switch>
+                <Route path={'/login'}
+                       render={() => <Login/>}/>
+                <Route path={'/profile'}
+                       render={() => <Profile/>}/>
+                <Route path={'/registration'}
+                       render={() => <Registration/>}/>
+                <Route path={'/restorpassword'}
+                       render={() => <RestorPassword/>}/>
+                <Route path={'/testPage'}
+                       render={() => <TestPage/>}/>
+                <Route path={'/inputpassword'}
+                       render={() => <InputPassword/>}/>
+                <Route path={'*'}
+                       render={() => <Error404/>}/>
+            </Switch>
+
+            <SuperInputText/>
+            <SuperCheckbox/>
+            <SuperButton/>
+        </div>
+    );
 }
 
 export default App;
