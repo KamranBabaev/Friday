1d62ffbeb9baa86feefb26483a84ccdeb3c6a796
boikopetro <boikopetromail@gmail.com>
2021-08-21 14:11:32 +0300

    add input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,42 @@
 import './App.css';
 import SuperInputText
-  from './components/common/c1-SuperInputText/SuperInputText';
+    from './components/common/c1-SuperInputText/SuperInputText';
 import SuperCheckbox from './components/common/c3-SuperCheckbox/SuperCheckbox';
 import SuperButton from './components/common/c2-SuperButton/SuperButton';
 import {Route, Switch} from 'react-router-dom';
 import {Login} from './components/Login/Login';
 import {Profile} from './components/Profile/Profile';
 import {Registration} from './components/Registration/Registration';
-import {RestorPassword} from './components/RestorPassword/RestorPassword';
+import {RestorePassword} from './components/RestorePassword/RestorePassword';
 import {TestPage} from './components/TestPage/TestPage';
 import {InputPassword} from './components/InputPassword/InputPassword';
 import {Error404} from './components/Error404/Error404';
 import {Header} from "./components/Header/Header";
 import SuperInput from "./components/common/c1-SuperInputText/SuperInputText";
 
-function App() {
-  return (
-      <div className="App">
-        <Header/>
-        <Switch>
-          <Route path={'/login'}
-                 render={() => <Login/>}/>
-          <Route path={'/profile'}
-                 render={() => <Profile/>}/>
-          <Route path={'/registration'}
-                 render={() => <Registration/>}/>
-          <Route path={'/restorpassword'}
-                 render={() => <RestorPassword/>}/>
-          <Route path={'/testPage'}
-                 render={() => <TestPage/>}/>
-          <Route path={'/inputpassword'}
-                 render={() => <InputPassword/>}/>
-          <Route path={'*'}
-                 render={() => <Error404/>}/>
-        </Switch>
 
-         <SuperInput inputText="some text" inputHeader={"login"}/>
-      </div>
-  );
+function App() {
+    return (
+        <div className="App">
+            <Header/>
+            <Switch>
+                <Route path={'/login'}
+                       render={() => <Login/>}/>
+                <Route path={'/profile'}
+                       render={() => <Profile/>}/>
+                <Route path={'/registration'}
+                       render={() => <Registration/>}/>
+                <Route path={'/restorpassword'}
+                       render={() => <RestorePassword/>}/>
+                <Route path={'/testPage'}
+                       render={() => <TestPage/>}/>
+                <Route path={'/inputpassword'}
+                       render={() => <InputPassword/>}/>
+                <Route path={'*'}
+                       render={() => <Error404/>}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/RestorePassword/RestorePassword.tsx
@@ -0,0 +1,15 @@
+import SuperInput from "../common/c1-SuperInputText/SuperInputText";
+
+export const RestorePassword = () => {
+    return(
+        <div>
+            <h1>Restore Password</h1>
+            <h2>Forgot your password?</h2>
+            <SuperInput inputHeader={"Email"} inputText={""}/>
+            <p>Enter your email address and we will send you further instruction</p>
+            <button>SuperButton</button>
+            <p>Did you remember your password?</p>
+            <a href=""> Try loginning in</a>
+        </div>
+    )
+}
