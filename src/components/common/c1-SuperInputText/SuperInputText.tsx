7dea195c10f2542086c8c618bd02abff300651f7
Kamran Babaev <babaevkamran2094@gmail.com>
2021-08-21 14:35:13 +0300

    kamran3

--- src/components/common/c1-SuperInputText/SuperInputText.tsx
+++ src/components/common/c1-SuperInputText/SuperInputText.tsx
@@ -1,9 +1,12 @@
 
+type PropsType = {
+    inputHeader: string
+    inputText: string
+}
 
-
-const SuperInputText = () => {
+const SuperInputText = (props: PropsType) => {
     return (
-        <div>1</div>
+        <div>{props.inputHeader}</div>
     )
 }
 
