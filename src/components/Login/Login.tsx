dce234f08c5300e5c523a1c446339a64e6ccbbc0
boikopetro <boikopetromail@gmail.com>
2021-08-25 17:47:17 +0300

    validation refactored

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -14,91 +14,97 @@ import {validateEmail} from "../common/validation/emailValidation";
 
 export const Login = () => {
 
-  const dispatch = useDispatch()
-  const authMe = useSelector<AppRootStateType, boolean>(state => state.login.authMe)
-  const [initialized, setInitialized] = useState(false)
-  const [openPassword, setOpenPassword] = useState(false)
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [checked, setChecked] = useState(false)
+    const dispatch = useDispatch()
+    const authMe = useSelector<AppRootStateType, boolean>(state => state.login.authMe)
+    const [initialized, setInitialized] = useState(false)
+    const [openPassword, setOpenPassword] = useState(false)
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [checked, setChecked] = useState(false)
+    const [disabledBtn, setDisabledBtn] = useState(true)
 
-  const changeViewPassword = () => {
-    setOpenPassword(!openPassword)
-  }
+    const changeViewPassword = () => {
+        setOpenPassword(!openPassword)
+    }
 
-  const emailTarget = (e: ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.currentTarget.value)
-  }
+    const emailTarget = (e: ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.currentTarget.value)
+    }
 
-  const passwordTarget = (e: ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.currentTarget.value)
-  }
+    const passwordTarget = (e: ChangeEvent<HTMLInputElement>) => {
+        if (validateEmail(email) && (e.currentTarget.value.length > 7)) {
+            setDisabledBtn(false)
+        } else {
+            setDisabledBtn(true)
+        }
+        setPassword(e.currentTarget.value)
+    }
 
-  const loginHandler = () => {
-    setInitialized(true)
-    setTimeout(() => dispatch(loginTC(email, password, checked)), 1000)
-    setEmail('')
-    setPassword('')
-    setChecked(false)
-  }
+    const loginHandler = () => {
+        setInitialized(true)
+        setTimeout(() => dispatch(loginTC(email, password, checked)), 1000)
+        setEmail('')
+        setPassword('')
+        setChecked(false)
+    }
 
-  if (authMe) {
-    return <Redirect to={'/'}/>
-  }
+    if (authMe) {
+        return <Redirect to={'/'}/>
+    }
 
-  return (
-      <div className={stylesContainer.container}>
 
-        <div className={stylesContainer.titleApp}>
-          <h1>Brain storm</h1>
-          <h2>Sign in</h2>
+    return (
+        <div className={stylesContainer.container}>
+            <div className={stylesContainer.titleApp}>
+                <h1>Brain storm</h1>
+                <h2>Sign in</h2>
+            </div>
+            {initialized && <Preloader/>}
+            <form className={stylesContainer.form}>
+                <div className={stylesContainer.item}>
+                    <p>Email:</p>
+                    <div className={stylesContainer.inputBlock}>
+                        <input onChange={emailTarget}
+                               value={email}
+                               type="text"
+                               placeholder="example@ddd.com"/>
+                    </div>
+                    {(email && (!validateEmail(email))) ?
+                        <div className={styles.errorMessage}>incorrect
+                            email!!!</div> : <></>}
+                    {(email && (validateEmail(email))) ?
+                        <div className={styles.correctMessage}>correct
+                            email</div> : <></>}
+                </div>
+                <div className={stylesContainer.item}>
+                    <p>Password:</p>
+                    <div className={stylesContainer.inputBlock}>
+                        <input onChange={passwordTarget}
+                               value={password}
+                               type={openPassword ? "text" : "password"}
+                               placeholder="****"/>
+                        <img onClick={changeViewPassword} alt=''
+                             src={openPassword ? eye : closedEye}/>
+                    </div>
+                    {(password) && (password.length < 8) ?
+                        <div className={styles.errorMessage}>password should be more
+                            than 7 symbols!!!</div> : <></>}
+                    {(password) && (password.length > 7) ?
+                        <div className={styles.correctMessage}>correct
+                            password</div> : <></>}
+
+                </div>
+                <div className={styles.rememberMe}>
+                    <input onClick={() => setChecked(!checked)}
+                           className={styles.checkbox}
+                           type="checkbox"/>
+                    <span>remember me</span>
+                </div>
+                <SuperButton
+                    disabledBtn={disabledBtn}
+                    title="Login"
+                    onClickHandler={loginHandler}/>
+            </form>
         </div>
-        {initialized && <Preloader/>}
-        <form className={stylesContainer.form}>
-          <div className={stylesContainer.item}>
-            <p>Email:</p>
-            <div className={stylesContainer.inputBlock}>
-              <input onChange={emailTarget}
-                     value={email}
-                     type="text"
-                     placeholder="example@ddd.com"/>
-            </div>
-            {(email && (!validateEmail(email))) ?
-                <div className={styles.errorMessage}>incorrect
-                  email!!!</div> : <></>}
-            {(email && (validateEmail(email))) ?
-                <div className={styles.correctMessage}>correct
-                  email</div> : <></>}
-          </div>
-          <div className={stylesContainer.item}>
-            <p>Password:</p>
-            <div className={stylesContainer.inputBlock}>
-              <input onChange={passwordTarget}
-                     value={password}
-                     type={openPassword ? "text" : "password"}
-                     placeholder="****"/>
-              <img onClick={changeViewPassword} alt=''
-                   src={openPassword ? eye : closedEye}/>
-            </div>
-            {(password) && (password.length < 8) ?
-                <div className={styles.errorMessage}>password should be more
-                  than 7 symbols!!!</div> : <></>}
-            {(password) && (password.length > 7) ?
-                <div className={styles.correctMessage}>correct
-                  password</div> : <></>}
-
-          </div>
-          <div className={styles.rememberMe}>
-            <input onClick={() => setChecked(!checked)}
-                   className={styles.checkbox}
-                   type="checkbox"/>
-            <span>remember me</span>
-          </div>
-          <SuperButton
-              disabledBtn={validateEmail(email) && (password.length < 8)}
-              title="Login"
-              onClickHandler={loginHandler}/>
-        </form>
-      </div>
-  )
+    )
 }
