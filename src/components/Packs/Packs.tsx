5778f0ce8e2c9da3e1c3d7204dd707af8e76ed6c
rarbar <senloren@yandex.ru>
2021-08-30 20:00:53 +0300

    +1

---
+++ src/components/Packs/Packs.tsx
@@ -0,0 +1,36 @@
+import {useDispatch, useSelector} from 'react-redux';
+import {AppRootStateType} from '../../redux/store';
+import {useEffect} from 'react';
+import {fetchCardsTC} from '../../redux/reducers/reducerCards';
+import styles from './packs.module.css'
+
+
+export const Packs = () => {
+    const cards = useSelector<AppRootStateType, string>(state => state.cards)
+    const dispatch = useDispatch()
+
+
+    useEffect(() => {
+        dispatch(fetchCardsTC('612c9b759f1a7900041d6f25'))
+    }, [])
+
+    return (
+        <div className={styles.packs}>
+            <div>
+                <h2>Show packs cards</h2>
+                <button>My</button>
+                <button>All</button>
+                <p>Number of cards</p>
+                <p>1-100</p>
+            </div>
+            <div>
+                <h1>Packs list</h1>
+                <div>
+                    <input/>
+                    <button>Add new pack</button>
+                </div>
+                <div>{cards}</div>
+            </div>
+        </div>
+    )
+}
